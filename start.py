3f1e2fab3247728c21b85ef057d5c5a19c092835
Doerp <felix@doerpmund.de>
2020-02-09 23:19:58 +0100

    added requirements and additional script

---
+++ start.py
@@ -0,0 +1,90 @@
+import speech_recognition as sr
+import datetime
+import nltk
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+
+class init_data:
+
+    """
+    used for initialising data and preprocessing tasks
+    """
+
+    def __init__(self, input = None):
+
+        self.input = input
+        self.text = None
+        self.raw_text = None
+        self.tagged = None
+        self.timestamp = None
+
+    def recognise_input(self):
+        """
+        recognise what kind of input we are getting. to be continued
+        :return:
+        """
+        # is it live or not live?
+        mode = "live"
+        speech = True
+
+        self.mode = mode
+        self.speech = speech
+
+        return
+
+    def import_input(self):
+        """
+        importing the input properly and deriving textual data from it
+        :return:
+        """
+
+        if self.speech is True and self.mode == "live":
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                print("Speak Anything :")
+                r.adjust_for_ambient_noise(source)
+                self.audio = r.listen(source)
+
+            try:
+                text = r.recognize_google(self.audio)
+                print("You said : {}".format(text))
+                self.raw_text = text
+
+                # save the text for the collection in mongoDB
+                self.timestamp = str(datetime.datetime.now())
+
+                return
+
+            except:
+                print("Sorry could not recognize your voice")
+                return
+
+        else:
+            pass
+
+        return
+
+    def preprocess(self):
+        """
+        apply nlp preprocessing steps to ensure proper structure of data
+        :return:
+        """
+
+        #translate to target language here - we can recognise different languages in the recognise_google function
+
+        #remove stop words - these are typically not important - we lose negations though :/ lets not do that for the moment
+
+        #words should be transformed into tokens to work with in nlp
+        self.text = word_tokenize(self.raw_text)
+
+        #we need to address the part of speech tag for later wordnet usage
+        self.tagged = pos_tag(self.text)
+
+        #we will lowercase them all to make them comparable and matchable
+        self.text = [token.lower() for token in self.text]
+
+        return
+
